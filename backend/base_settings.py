58256d441c099f4c9c232a4f8ecc81e546098e35
Habimana Gilbert <habilbert@gmail.com>
2018-08-14 12:43:08 +0200

    after billing

--- backend/base_settings.py
+++ backend/base_settings.py
@@ -78,8 +78,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'backend.urls'
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:8000',
-    '127.0.0.1:8000',
+    'localhost:3000',
+    '127.0.0.1:3000',
 )
 
 TEMPLATES = [
